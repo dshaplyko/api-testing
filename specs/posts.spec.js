97919c109e1ba3e88e1b06f20bc809f133ed52b4
Dmitry Shaplyko <dzmitry_shaplyka@epam.com>
2017-09-21 12:35:32 +0300

    Project created

---
+++ specs/posts.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const chakram = require('chakram');
+const expect = chakram.expect;
+const api = require('./utils/api');
+const data = require('../server/data.json');
+
+describe('Posts', () => {
+});
